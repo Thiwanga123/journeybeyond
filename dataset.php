ae300962a30e71078a82627a0bcde10cf9a6a8b7
Thiwanga123 <info.thiwangasri@gmail.com>
2024-08-24 18:14:14 +0530

    delete unnecessary css codes

--- dataset.php
+++ dataset.php
@@ -9,9 +9,7 @@ $bottomText;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dataset</title> -->
+   
     <style>
         .Datasection{
             width:250px;
